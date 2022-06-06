7f1e1806928f3c4bb0d28bb31cabafb3546de94a
unknown <davinakmal856@gmail.com>
2022-06-06 12:19:55 +0700

    teste

---
+++ resources/views/tambahproduk.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.templatetable')
+@section('page-title')
+    Tambah Produk
+@endsection
+@section('table')
+    <div class="container">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form action="/simpanproduk" method="post">
+            @csrf
+            <div class="modal-body">
+                Nama:
+                <input type="text" name="name" id="" class="form-control">
+                deskripsi:
+                <input type="text" name="description" id="" class="form-control">
+                harga:
+                <input type="text" name="price" id="" class="form-control">
+                kategori:
+                <select name="category_id" id="" class="form-control">
+                @foreach ( $category as $d )
+                    <option value="{{ $d->id}}">{{$d->name}}</option>
+                @endforeach
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Submit!</button>
+
+            </div>
+
+        </form>
+    @endsection
